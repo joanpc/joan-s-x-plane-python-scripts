996b7a8d8f3325cea22e3a085cac506410c68a98
Joan Perez i Cauhe <joanpc@gmail.com>
2021-05-07 17:03:14 +0200

    1.2 installer version.

--- PythonScriptsInstaller.py
+++ PythonScriptsInstaller.py
@@ -1,24 +1,45 @@
 #!/usr/bin/env python
+'''
+Joan's x-plane Python Plugins Net installer
+
+Copyright (C) 2012-2015 Joan Perez i Cauhe
+---
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+'''
 import Tkinter as tk
 import tkFileDialog
 import threading
+import ttk
 import time
 
 import json
 import urllib
+import ssl
 import tempfile
-import os
+import platform
 from zipfile import ZipFile
 from shutil import move, rmtree, copytree, copy
 from time import sleep
 import tempfile
+import ssl
 
 import tkFont
+import sys
+import os
+
+VERSION = '1.2'
 
 class XPScriptsUpdater:
     ''' Process script updates
     '''
-    VERSION = '0.1'
     UPDATE_URL='http://x-plane.joanpc.com/plugins/updater_json/OSX/' + VERSION
 
     updates = []
@@ -28,6 +49,8 @@ class XPScriptsUpdater:
         self.xplanedir = xplanedir
         self.processing = False
         self.printStatus = False
+        self.downloadProgress = False
+        self.errors = 0
         pass
 
     def tcopy(self, src, dst):
@@ -58,7 +81,7 @@ class XPScriptsUpdater:
 
             self.printStatus('Installing files:')
             copy(dpath + '/'  +  update['update_filename'], installpath + '/'  +  update['update_filename'])
-            self.printStatus('.', False)
+            self.printStatus(' ok.', 'bold', False)
 
             return True
 
@@ -66,13 +89,8 @@ class XPScriptsUpdater:
             zipfile = dpath + '/._xpjpcUPDATE.zip'
             # Download update
 
-            self.printStatus('Downloading: \n%s' % update['update_url'])
-            try:
-                urllib.urlretrieve(update['update_url'], zipfile)
-            except:
-                self.printStatus('ERROR: ')
+            if not self.download(update['update_url'], zipfile):
                 return False
-            self.printStatus('.', False)
 
             zip = ZipFile(zipfile, 'r')
 
@@ -87,7 +105,7 @@ class XPScriptsUpdater:
                 self.tcopy(unzipdir, installpath)
                 rmtree(unzipdir)
                 os.remove(zipfile)
-            self.printStatus('.', False)
+            self.printStatus(' ok.', 'bold', newline = False)
 
             return True
 
@@ -104,6 +122,8 @@ class XPScriptsUpdater:
                 try:
                     os.mkdir(path)
                 except:
+                    self.printStatus('ERROR: %s%s' % (sys.exc_info()[0], sys.exc_info()[1]), 'error')
+                    self.errors += 1
                     return False
         return True
 
@@ -111,11 +131,12 @@ class XPScriptsUpdater:
 
         self.printStatus('Downloading: \n%s' % url)
         try:
-            urllib.urlretrieve(url, path)
+            urllib.urlretrieve(url, path, reporthook = self.downloadProgress, context=ssl._create_unverified_context())
         except:
-            self.printStatus(' ERROR!')
+            self.printStatus('ERROR: %s%s' % (sys.exc_info()[0], sys.exc_info()[1]), 'error')
+            self.errors += 1
             return False
-        self.printStatus('.', False)
+        self.printStatus(' ok.', 'bold', newline = False)
 
         return path
 
@@ -143,8 +164,9 @@ class XPScriptsUpdater:
     def findUpdates(self, signature = False, version = False):
 
         try:
-            res = urllib.urlopen(self.UPDATE_URL)
+            res = urllib.urlopen(self.UPDATE_URL, context=ssl._create_unverified_context())
         except:
+            self.errors += 1
             return False
 
         if not res:
@@ -183,16 +205,49 @@ class Application(tk.Frame):
         self.updater.printStatus = self.printStatus
         self.processing = False
         self.createWidgets()
+        self.disableCertVerify()
 
         self.updates = False
-        self.printStatus('Checking the net for available scripts.')
+        self.printStatus('Checking the net for available scripts:', newline = False)
         self.threadDo(self.findUpdates, None)
-        self.printStatus('done.')
+        self.printStatus(' ok.', 'bold', newline = False)
+        self.checkPython()
+        self.printStatus('Please select which plugins to install and your x-plane folder.')
 
         if self.updates:
             self.createUpdatesWidgets(self.updates_frame)
         else:
-            self.printStatus('ERROR: downloading updates\nCheck yout internet connection and try again.')
+            self.printStatus('ERROR: downloading updates.\nThis installer needs an internet connection.\nCheck your network connection and try again.', 'error')
+            self.installButton.config(text = 'Quit', command = self.close)
+
+    def track(self, errors):
+        try:
+            urllib.urlopen('http://analytics.joanpc.com/piwik.php?action_name=win_install&idsite=4&rec=1&send_image=0&url=http://x-plane.joanpc.com/OSXInstaller/install/%s/errors/%d' %
+        VERSION, errors, context=ssl._create_unverified_context())
+        except:
+            pass
+
+    def checkPython(self):
+
+        if sys.platform.lower() == 'darwin':
+            major, minor = platform.mac_ver()[0].split('.')[:2]
+            if int(major) == 10 and int(minor) < 7:
+                self.printStatus('WARNING: Your osx version doesn\'t ship with a 2.7 python installation.', 'warning')
+                self.printStatus('Download and isntall Python 2.7 from http://www.python.com.', 'warning')
+                return False
+            else:
+                self.printStatus('Your OSX version comes with Python 2.7 installed.', 'bold')
+                return True
+        elif sys.platform.lower() == 'win32':
+            # Windows
+            pass
+
+
+    def sslContext(self, purpose=ssl.Purpose.SERVER_AUTH, cafile=None, capath=None, cadata=None):
+        return ssl.create_default_context(cafile = 'certs.pem')
+
+    def disableCertVerify(self):
+        ssl._create_default_https_context  = self.sslContext
 
     def findUpdates(self, ignoreme):
         self.updates = self.updater.findUpdates()
@@ -211,7 +266,6 @@ class Application(tk.Frame):
         if self.updates:
             for sid, data in self.updates.iteritems():
                 tk.Label(frame, text = data['name']).grid(row = row, column = 0, sticky = "W")
-
                 self.installChecks[sid] = tk.IntVar()
                 check = tk.Checkbutton(frame, variable = self.installChecks[sid])
                 check.grid(row = row, column = 1)
@@ -219,7 +273,7 @@ class Application(tk.Frame):
                 row += 1
 
         else:
-            self.printStatus('No updates available')
+            self.printStatus('ERROR: No updates available', 'error')
 
 
     def separator(self, frame):
@@ -254,31 +308,49 @@ class Application(tk.Frame):
             command=self.doInstalls)
         self.installButton.pack(side = 'right')
 
-        self.statusCaption = tk.StringVar()
-        tk.Label(self, textvariable = self.statusCaption, font = ('monospace'),
-            height = 8, anchor = 'sw', justify = tk.LEFT, width = 60,
-            padx = 5, pady = 5, bg = '#fff').pack(fill = 'x')
-        self.printStatus('Please, select your X-Plane folder.')
+        self.progress = tk.IntVar()
+        self.progressbar = ttk.Progressbar(self, variable = self.progress)
+        self.progressbar.pack(fill = 'both')
 
-        #print row
-        #self.quitButton = tk.Button(self, text='Exit',
-        #    command=self.quit)
-        #self.quitButton.grid(row = row, column = 1, columnspan = 1, sticky = 'W')
+        self.updater.downloadProgress = self.downloadProgress
+
+        frame = tk.Frame(self)
+        frame.pack(fill = 'x')
+
+        self.statusScroll = tk.Scrollbar(frame)
+        self.statusScroll.pack(side = 'right', fill = 'y')
+
+        self.status = tk.Text(frame, height = 8, padx = 5, pady = 5,
+            state = 'disabled', bg = '#fff', yscrollcommand = self.statusScroll.set)
+        self.status.tag_config("error", foreground="red")
+        self.status.tag_config('warning', foreground='DarkOrange3')
+        self.status.tag_config("bold", foreground="#006600")
+        self.status.pack(side = 'left', fill = 'both')
+
+        self.statusScroll.config(command = self.status.yview)
+
+    def downloadProgress(self, count, blockSize, totalSize):
+        self.progress.set( count * blockSize * 100 / totalSize )
 
     def selectXplaneFolder(self):
         if not self.processing:
             self.xp_path = tkFileDialog.askdirectory(title = 'Pick a folder', initialdir = '/')
             if self.xp_path:
-                self.printStatus('Selected X-Plane folder:\n %s' % (self.xp_path))
+                self.printStatus('Selected X-Plane folder:\n%s' % (self.xp_path))
             else:
-                self.printStatus('No folder selected.')
+                self.printStatus('No folder selected.', 'error')
 
-    def printStatus(self, message, newline = True):
+    def printStatus(self, message, tag = None, newline = True):
         nl = ''
         if newline:
             nl = "\n"
 
-        self.statusCaption.set('%s%s%s' % (self.statusCaption.get(), nl, message))
+        self.status.config(state='normal')
+        self.status.insert('end', nl + message, tag)
+        self.status.see('end')
+        self.status.config(state='disabled')
+
+        #self.statusCaption.set('%s%s%s' % (self.statusCaption.get(), nl, message))
 
     def threadDo(self, task, args):
         # Run a task on a thread while updating.
@@ -289,18 +361,24 @@ class Application(tk.Frame):
         th.start()
         while th.is_alive():
             window.update()
-            time.sleep(0.001)
+            th.join(0.001)
+
+    def close(self):
+        if self.processing:
+            self.printStatus('Still working, please wait', 'error')
+        else:
+            self.winfo_toplevel().destroy()
 
     def doInstalls(self):
 
         if not self.xp_path:
-            self.printStatus('ERROR: No X-Plane folder selected.')
+            self.printStatus('ERROR: No X-Plane folder selected.', 'error')
             return
         else:
             self.updater.xplanedir = self.xp_path
 
         if not self.updater.checkPaths():
-            self.printStatus('ERROR: Accessing x-plane folder.')
+            self.printStatus('ERROR: Accessing x-plane folder.', 'error')
             return
 
         if self.processing:
@@ -310,22 +388,36 @@ class Application(tk.Frame):
         self.processing = True
 
         if self.installPythonInteface.get():
-            self.printStatus('Installing %s' % 'Python Interface: ')
+
+            self.printStatus('--\nInstalling %s' % 'Python Interface: ', 'bold')
             self.threadDo(self.updater.installPlugin, ['url'])
-            self.printStatus('Completed.', None)
+            self.printStatus('Installation Completed.', 'bold')
 
         for sid, install in self.installChecks.iteritems():
             if install.get():
 
-                self.printStatus('Installing %s: ' % (self.updates[sid]['name']))
+                self.progress.set(0)
+                self.progressbar.start(1000)
+                self.printStatus('--\nInstalling %s: ' % (self.updates[sid]['name']))
                 self.threadDo(self.updater.update, self.updates[sid])
-                self.printStatus(' Done.', False)
-
+                self.printStatus('Installation Completed.', 'bold')
+                self.progressbar.stop()
+                self.progress.set(100)
                 self.winfo_toplevel().update()
 
-        self.printStatus('ALL installations COMPLETED.')
+        self.printStatus('-- \nALL installations COMPLETED.', 'bold')
+        if self.updater.errors:
+            self.printStatus('%d ERRORS during installation.' % (self.updater.errors), 'error')
+        self.printStatus('-- \nThis installer is in development:\nPlease report your experience to x-plane@joanpc.com')
         self.processing = False
+        self.track(self.updater.errors)
+        self.installButton.config(text = 'Quit', command = self.close)
 
 app = Application()
-app.master.title('X-Plane Python Scripts Net Installer')
+root = app.winfo_toplevel()
+app.master.title('X-Plane Python Scripts Net Installer %s' % (VERSION))
+root.protocol("WM_DELETE_WINDOW", app.close)
+root.resizable(0,0)
+root.call('wm', 'attributes', '.', '-topmost', True)
+root.after_idle(root.call, 'wm', 'attributes', '.', '-topmost', False)
 app.mainloop()
